068858c4e043f3173105f3c1ec185d98cf391d2b
Marilynmonroy <artlyn.monroy@gmail.com>
2023-12-05 13:35:26 -0300

    1-push

---
+++ prisma/migrations/20231205044747_init/migration.sql
@@ -0,0 +1,36 @@
+-- CreateTable
+CREATE TABLE "cliente" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "interestId" INTEGER NOT NULL,
+    "businessId" INTEGER NOT NULL,
+
+    CONSTRAINT "cliente_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "intereses" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "intereses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "area-de-negocio" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "area-de-negocio_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "cliente_email_key" ON "cliente"("email");
+
+-- AddForeignKey
+ALTER TABLE "cliente" ADD CONSTRAINT "cliente_interestId_fkey" FOREIGN KEY ("interestId") REFERENCES "intereses"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "cliente" ADD CONSTRAINT "cliente_businessId_fkey" FOREIGN KEY ("businessId") REFERENCES "area-de-negocio"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
